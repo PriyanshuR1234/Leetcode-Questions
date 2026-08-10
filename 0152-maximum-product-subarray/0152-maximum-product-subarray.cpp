95c7d6ae11206f2794703b8bcfda58d305276cc9
Priyanshu Rajput <140266349+PriyanshuR1234@users.noreply.github.com>
2026-08-10 18:46:41 +0530

    Time: 11 ms (9.6%), Space: 20.8 MB (5.86%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,20 +2,47 @@ class Solution {
 public:
     
     int maxProduct(vector<int>& nums) {
-        int maxi=INT_MIN;
-        int prefix=1;
-        int suffix=1;
+
+
+        //most optimized tricky
+        
+        // int maxi=INT_MIN;
+        // int prefix=1;
+        // int suffix=1;
+        // int n=nums.size();
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(prefix==0)prefix=1;
+        //     if(suffix==0)suffix=1;
+
+        //     prefix=prefix*nums[i];
+        //     suffix=suffix*nums[n-1-i];
+
+        //     maxi=max(maxi,max(prefix,suffix));
+        // }
+        // return maxi;
+
+        //dp solution
+        if(nums.empty())return 0;
         int n=nums.size();
-        for(int i=0;i<nums.size();i++)
+
+        int global=nums[0];
+        vector<vector<int >>dp(n,vector<int>(2,0));
+
+        dp[0][0]=nums[0];
+        dp[0][1]=nums[0];
+
+        for(int i=1;i<n;i++)
         {
-            if(prefix==0)prefix=1;
-            if(suffix==0)suffix=1;
+            int val=nums[i];
 
-            prefix=prefix*nums[i];
-            suffix=suffix*nums[n-1-i];
+            dp[i][0]=max({val,val*dp[i-1][0],val*dp[i-1][1]});
 
-            maxi=max(maxi,max(prefix,suffix));
+            dp[i][1]=min({val,val*dp[i-1][0],val*dp[i-1][1]});
+
+            global=max(global,dp[i][0]);
+
         }
-        return maxi;
+        return global;
     }
 };
