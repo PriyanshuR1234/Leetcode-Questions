27aa58ca5284c7922faffb3cbeb74678c562df4b
Priyanshu Rajput <140266349+PriyanshuR1234@users.noreply.github.com>
2026-08-21 15:28:06 +0530

    Time: 0 ms (100%), Space: 8.1 MB (57.53%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-         if (s.length() != goal.length()) {
-            return false;
-        }
-        
-        // Doubling s contains all possible rotations
-        string doubled = s + s;
-        
-        // Check if goal is a substring of doubled
-        return doubled.contains(goal);
+        if(s.length()!=goal.length())return false;
+        string ss=s+s;
+        return ss.contains(goal);
     }
 };
