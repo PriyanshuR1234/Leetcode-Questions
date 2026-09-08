aaeabfd3d40b5fc8f7f3c87d0e49c20413e0505a
Priyanshu Rajput <140266349+PriyanshuR1234@users.noreply.github.com>
2026-09-08 13:57:28 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.49%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int digit=0;
-        int hundred=0;
         int num=n;
-        while(n>0)
-        {
-            digit++;
-            n=n/10;
-        }
-        cout<<digit;
-        if(digit<=3) return hundred;
-        if(digit>=3)
-        {
-            hundred=1;
-            digit-=3;
-        }
+        // int digit=0;
+        // int hundred=0;
+        
+        // while(n>0)
+        // {
+        //     digit++;
+        //     n=n/10;
+        // }
+        // cout<<digit;
+        // if(digit<=3) return hundred;
+        // if(digit>=3)
+        // {
+        //     hundred=1;
+        //     digit-=3;
+        // }
+        if(n<1000)return 0;
         
 
         
