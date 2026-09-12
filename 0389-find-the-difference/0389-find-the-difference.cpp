6a511013ee86571054298e5f26d1e721c5179976
Priyanshu Rajput <140266349+PriyanshuR1234@users.noreply.github.com>
2026-09-12 23:49:47 +0530

    Time: 0 ms (100%), Space: 9.2 MB (50.94%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char, int> arr;
-        for (char c : s) {
-            arr[c]++;
+        // unordered_map<char, int> arr;
+        // for (char c : s) {
+        //     arr[c]++;
+        // }
+        // for (char c : t) {
+        //     arr[c]--;
+        //     if (arr[c] < 0) { 
+        //         return c;
+        //     }
+        // }
+        // return ' ';
+
+        char res=0;
+        for(int i=0;i<s.size();i++)
+        {
+            res^=s[i];
+            res^=t[i];
         }
-        for (char c : t) {
-            arr[c]--;
-            if (arr[c] < 0) { 
-                return c;
-            }
-        }
-        return ' ';
+        res^=t[t.size()-1];
+        return res;
     }
 };
