d28d9b668a3fd11039f2f4dd3884005667a03941
Priyanshu Rajput <140266349+PriyanshuR1234@users.noreply.github.com>
2026-09-15 14:49:16 +0530

    Time: 3 ms (37.94%), Space: 8.9 MB (36.6%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     int arrangeCoins(int n) {
         if(n==1)return 1;
-        int count=0;
         int i=1;
         while(n-i>=0)
         {
             n=n-i;
             i++;
-            count++;
         }
-        return count;
+        return i-1;
     }
 };
