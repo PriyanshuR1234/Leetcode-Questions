f84b65c19598cc8f006866b3547e232d119afe9c
Priyanshu Rajput <140266349+PriyanshuR1234@users.noreply.github.com>
2026-09-17 10:32:25 +0530

    Time: 0 ms (100%), Space: 11.6 MB (50.53%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -3,17 +3,27 @@ public:
     vector<int> decrypt(vector<int>& code, int k) {
         vector<int>ans(code.size(),0);
         if(k==0) return ans;
-        if(k<0)reverse(code.begin(),code.end());
+        //if(k<0)reverse(code.begin(),code.end());
         int n=code.size();
 
         for(int i=0;i<code.size();i++)
         {
-            for(int j=0;j<abs(k);j++)
+            if(k>0)
             {
-                ans[i]+=code[(i+j+1)%n];
+                for(int j=0;j<abs(k);j++)
+                {
+                    ans[i]+=code[(i+j+1)%n];
+                }
+            }
+            else
+            {
+                for(int j=0;j<abs(k);j++)
+                {
+                    ans[(i+1)%n]+=code[(i-j+n)%n];
+                }
             }
         }
-        if(k<0)reverse(ans.begin(),ans.end());
+        //if(k<0)reverse(ans.begin(),ans.end());
         return ans;
     }
 };
